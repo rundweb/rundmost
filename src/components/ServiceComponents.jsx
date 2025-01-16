83538fbb68534e599657547832d68dac56cd1507
faldo <pn4066283@gmail.com>
2025-01-16 20:13:07 +0700

    service mobile done 2

--- src/components/ServiceComponents.jsx
+++ src/components/ServiceComponents.jsx
@@ -38,22 +38,21 @@ const ServiceComponents = () => {
     },
   ];
   return (
-    <div className="bg-black-secondary mt-5">
-      <div className="px-5 max-w-7xl gap-10 m-auto py-12 flex flex-col items-center w-full">
-        <div className="flex flex-col gap-2 text-center">
-          <h1 className="text-white font-bold text-2xl">
+    <div className="bg-black-secondary mt-5 md:mt-10">
+      <div className="px-5 max-w-7xl gap-10 m-auto py-12 flex flex-col items-center w-full md:py-20">
+        <div className="flex flex-col gap-2 text-center items-center">
+          <h1 className="text-white font-bold text-2xl md:text-4xl md:w-3/5">
             Eficient and integrated manufacturing services
           </h1>
           <p className="text-white tracking-wide font-normal text-sm">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Distinctio, aut!
           </p>
         </div>
-        <div className="flex flex-col gap-10 justify-between  w-full">
+        <div className="flex flex-col gap-10 justify-around w-full sm:flex-row sm:flex-wrap">
           {service.map((item) => (
             <div
               key={item.title}
-              className="bg-service shadow-lg  w-full p-7 h-60  flex flex-col justify-between"
+              className="bg-service shadow-lg  w-full p-7 h-60  flex flex-col justify-between sm:w-72 lg:w-96"
             >
               <item.icon className="text-white text-5xl" />
               <div className="flex flex-col gap-1">
